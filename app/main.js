920717ae54bbfbbacd1b3222624c847451665496
selena1110 <1351017996@qq.com>
2017-04-21 09:22:58 +0800

    a

---
+++ app/main.js
@@ -0,0 +1,3 @@
+import './main.css';
+var greeter=require('./Greeter.js');
+document.getElementById("root").appendChild(greeter());
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports={
+	devtool:'eval-source-map',
+	entry:__dirname+"/app/main.js",
+	output:{
+		path:__dirname+"/public",
+		filename:"bundle.js"
+	},
+	module:{
+		loaders:[{
+			test:/\.css$/,
+           loader:'style-loader!css-loader'
+       }
+           
+		]
+	},
+	devServer:{
+		contentBase:"./public",
+		colors:true,
+		historyApiFallback:true,
+		inline:true
+	}
+}
